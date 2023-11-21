5e2b7e8241cc036567e7dc0b7d547bc247deae6b
zerK4 <sebastian.v.pavel@gmail.com>
2023-11-21 15:06:38 +0200

    prepare vercel deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import svelte from "@astrojs/svelte";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), svelte()],
   site: "https://zerK4.github.io",
   base: "/raian-astro",
+  output: "server",
+  adapter: vercel()
 });
