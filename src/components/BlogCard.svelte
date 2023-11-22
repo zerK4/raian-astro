5b75fb90a485a214219808e5f6db4452b285f270
zerK4 <sebastian.v.pavel@gmail.com>
2023-11-22 09:17:47 +0200

    add blog functionality

---
+++ src/components/BlogCard.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  export let blog: any;
+  const {
+    id,
+    attributes: {
+      name,
+      createdAt,
+      publishedAt,
+      content,
+      by,
+      date,
+      buttonName,
+      slug,
+    },
+  } = blog;
+</script>
+
+<div class="text-white p-2 border border-[#C78665] rounded-md w-fit">
+  <h2 class="">{name}</h2>
+  <a href={`/blog/${slug}`} class="">{buttonName}</a>
+</div>
---
+++ src/components/BlogContainer.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { marked } from "marked";
+  export let blog;
+
+  const { content } = blog;
+
+  console.log(blog, "the blog");
+</script>
+
+<div class=" text-white">
+  {@html marked(content)}
+</div>
